74cbc5c40dcf68acba2cbaae1e843883941ddd63
MarkvsLoopvs <aat_work@mail.ru>
2024-12-04 13:34:47 +0500

    Renaming, merged day 1 into one file

--- day01/day1.jl
+++ day01/day1.jl
@@ -5,7 +5,16 @@ lists = readdlm("input.txt", Int64)
 left_list = lists[:, 1]
 right_list = lists[:, 2]
 
+similarity_score = 0
+
+for element in left_list
+    count = sum(right_list .== element) 
+    global similarity_score += element * count
+end
+
 sort!(left_list)
 sort!(right_list)
 
 total_distance = sum(abs.(left_list .- right_list))
+println(total_distance)
+println(similarity_score)
